71df4a3be291a283fbf167bb73f2b6fc5aae89b9
Okiemute Gold <goldokiemute@gmail.com>
2022-06-08 00:05:41 +0100

    car details page added, and ActivatedRoute imported

--- src/app/all-services/all-cars/all-cars.service.ts
+++ src/app/all-services/all-cars/all-cars.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AllCarsService {
   getAllCars() {
@@ -10,70 +10,76 @@ export class AllCarsService {
         id: 1,
         name: 'Honda Accord',
         link: 'honda-accord',
-        tag: "Office car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "22000.564",
+        tag: 'Office car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '22000.564',
         manufacturer: 'Honda',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
       {
         id: 2,
         name: 'Venza',
         link: 'venza',
-        tag: "Family car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "12000.55",
+        tag: 'Family car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '12000.55',
         manufacturer: 'Toyota',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
       {
         id: 3,
         name: 'Toyota Avalon',
         link: 'toyota-avalon',
-        tag: "Classy car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "36000.855",
+        tag: 'Classy car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '36000.855',
         manufacturer: 'Toyota',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
       {
         id: 4,
         name: 'Rose Royce',
         link: 'rose-royce',
-        tag: "Sporty car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "45000.975",
+        tag: 'Sporty car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '45000.975',
         manufacturer: 'Toyota',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
       {
         id: 5,
         name: 'Camry',
         link: 'camry',
-        tag: "Family car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "17000.5",
+        tag: 'Family car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '17000.5',
         manufacturer: 'Toyota',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
       {
         id: 6,
         name: 'Porsche',
         link: 'porsche',
-        tag: "Sporty car",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.",
-        rating: "4.546",
-        price: "20000.750",
+        tag: 'Sporty car',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum architecto.',
+        rating: '4.546',
+        price: '20000.750',
         manufacturer: 'Toyota',
-        releaseDate: new Date(2022, 3, 14)
+        releaseDate: new Date(2022, 3, 14),
       },
-    ]
-  };
+    ];
+  }
 
-  constructor() { }
+  constructor() {}
 }
--- src/app/views/product-details/product-details.component.ts
+++ src/app/views/product-details/product-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AllCarsService } from '../../all-services/all-cars/all-cars.service';
+// import { Product, products } from '../products-page';
 
 @Component({
   selector: 'product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  allCars: any = [];
+  product: any | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, service: AllCarsService) {
+    this.allCars = service.getAllCars();
   }
 
+  ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = this.allCars.find(
+      (product: { id: number }) => product.id === productIdFromRoute
+    );
+  }
 }
